cabc33bea0782bc613aeb7511c7b6ea6673ad1fc
katz00 <>
2020-02-11 14:53:43 +0900

    Make a basic User model and chage gem to sqlite3 1.4.0

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+
+  before_save { email.downcase! }
+
+  validates :name, presence: true
+  validates :name, length: {maximum: 50}
+
+  validates :email, presence: true
+  validates :email, length:{ maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :email, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, length:{ minimum: 6}
+  validates :password, presence: true
+end
