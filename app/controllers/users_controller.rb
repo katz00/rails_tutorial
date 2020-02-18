c5fa79cb5627a9dcc4029345ffcfe2a4041b7170
katz00 <>
2020-02-18 10:56:03 +0900

    Finish add user delete function and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def new
     @user = User.new
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
     @users = User.page(params[:page]).per(30)
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "ユーザーを削除しました"
+    redirect_to root_url
+  end
+
   
   private
 
@@ -63,5 +70,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,35 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
   end
+
+  test "should not allow the admin attribute to be edit via the web" do
+    log_in_as(@other_user) #なぜ@other_userか、@userをa管理者dminにしているから
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params:{user:{password: "password",
+                      password_confirmation: "password", admin: true}}
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_path
+  end
+
+  test "should redirect destroy when logged in  as a not admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "success destroy when logged in as a admin" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+    assert_redirected_to root_url
+  end
 end
