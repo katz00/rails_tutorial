f2e1b9125bc969f6ac73ef6a4552d1cb929a357a
katz00 <>
2020-03-04 17:04:20 +0900

    Add validation to Relationship model #81

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+    @relationship = @user.active_relationships.build(follower_id: @user.id,
+                                      followed_id: @other_user.id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
