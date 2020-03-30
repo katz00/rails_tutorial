9b0778c8306600ccca7cd7da4dbd04b8cb9da9c2
katz00 <>
2020-03-30 12:20:59 +0900

    fix issue#85

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,10 @@
 class Relationship < ApplicationRecord
 
-  =begin(active_relationship)=end belongs_to :follower, class_name: "User"
-  =begin(passive_relationship)=end belogns_to :following, class_name: "User"
+  #active_relationshipが従属するモデル
+  belongs_to :follower, class_name: "User"
+  
+  #passive_relationsipsが従属するモデル
+  belogns_to :following, class_name: "User"
   
   validates :follower_id, presence: true
   validates :following_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
 
-  =begin(follower)=end has_many :active_relationships, class_name: "Relationship", foreign_key: "follower", dependent: :destroy
-  =begin(follower)=end has_many :followings, through: :active_relationships, source: :followings
-
-  =begin(following)=end has_many :passive_relationships, class_name: "Relationship", foreign_key: "following", dependent: :destroy
-  =begin(following)=end has_many :followers, through: :passive_relationsips, source: :follower
+  #follower(フォローされる側のユーザー)が持つ関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :following
+  
+  #following(フォローする側のユーザー)が持つ関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "following", dependent: :destroy
+  has_many :followers, through: :passive_relationsips, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token #仮想の属性(仮想のカラム)
 
