c23809d0cdf9428a4a3bbe34ff90f60d7db2d21a
katz00 <>
2020-03-30 15:11:32 +0900

    Input sample data of follow #86

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,9 +2,9 @@ class Relationship < ApplicationRecord
 
   #active_relationshipが従属するモデル
   belongs_to :follower, class_name: "User"
-  
+
   #passive_relationsipsが従属するモデル
-  belogns_to :following, class_name: "User"
+  belongs_to :following, class_name: "User"
   
   validates :follower_id, presence: true
   validates :following_id, presence: true
